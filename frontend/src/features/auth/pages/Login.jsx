cf85940ceff78deba10ecc5074e66c2c8e5daf26
Vishnu Singh <vs414447@gmail.com>
2026-05-12 05:56:38 +0530

    Login / Register api integration with frontend.

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-import "../styles/form.scss"
-import { Link } from 'react-router'
+import "../styles/form.scss";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+
+      e.target.reset();
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
-            // onInput={(e) => {
-            //   setUsername(e.target.value);
-            // }}
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
             type="text"
             name="username"
             placeholder="Enter username"
           />
           <input
-            // onInput={(e) => {
-            //   setPassword(e.target.value);
-            // }}
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
             name="password"
             placeholder="Enter password"
